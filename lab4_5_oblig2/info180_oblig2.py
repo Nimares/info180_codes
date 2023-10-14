653369b8ad0dbcaec599318fc0a73063ae868044
Nimares <nikolai.resvold@gmail.com>
2023-10-14 11:58:40 +0200

    fininished ML self learning file and assignment

--- lab4_5_oblig2/info180_oblig2.py
+++ lab4_5_oblig2/info180_oblig2.py
@@ -20,115 +20,111 @@ from sklearn.preprocessing import OrdinalEncoder
 import numpy as np
 from numpy import nan
 
+# 1. ----------------- Loading and preprocessing ------------------
+# 1.1 loading data
 
-#2. -----loading data-----
+# names for headers
 names = ['pregnancy_rate', 'glucose_concentration', 'blood_pressure', 'skin_thickness', 
          'insulin_levels','BMI', 'diabetes_inheretance_function', 'age', 'diabets_diagnosis' ]
 
+#load csv file with data
 dataset = read_csv('pima-indians-diabetes.csv', names=names)
 
-#.----summarizing data-----
+# 1.2 preprocessing.
 
-#3.1 Shape (instances(rows) and attributes(columns))
-#print(dataset.shape)
+# 1.2.1 finding missing values
+# statistical summary to look at an overview (some have val of 0)
+# print(dataset.describe())
 
-#3.2 peeking at data
-    #head = start from top of file, and how many rows
-#print(dataset.head(20))
-
-#3.3 statistical summary
-#print(dataset.describe())
-
-#3.4 class distribution
-    #number of rows with a given value
-#print(dataset.groupby('blood_pressure').size())
-
-
-#*-*-*-*- DEALING WITH MISSING VALUES *-*-*-*-*-
-#1. ----- finding missing values -----
-#1.1 using statistical summary to look at an overview (some have val of 0)
-#print(dataset.describe())
-
-#1.2finding number of missing values for each column
+# 1.2.2. finding number of missing values for each column
 # num_missing = (dataset[['glucose_concentration', 'blood_pressure', 'skin_thickness', 
 #          'insulin_levels','BMI']] == 0).sum()
+# print(num_missing)
 
-#print(num_missing)
-
-#1.3.1 marking missing values as NaN
+#1.2.3 marking missing values as nan
 dataset[['glucose_concentration', 'blood_pressure', 'skin_thickness', 
          'insulin_levels','BMI']] = dataset[['glucose_concentration', 'blood_pressure', 'skin_thickness', 
          'insulin_levels','BMI']].replace(0, nan)
 
-#1.3.2 overview of number of rows with nan values
+# 1.2.4 overview of number of rows with nan values
 # print(dataset.isnull().sum())
 
-#1.3.2 confirming replacements (no 0's only nan shown)
+# 1.2.5 confirming replacements (no 0's only nan shown)
 # print(dataset.head(30))
 
 
-#2. ----- removing rows with missing values -----
+# 1.2.6 removing rows with missing values
 # print(dataset.shape)
 dataset.dropna(inplace=True)
 # print(dataset.shape)
 
 
-#*-*-*-*- Evaluating models *-*-*-*-*-
-#5.1 split out validation dataset
-
+# 2. ------------- Splitting data into test and training -------------------------
 # returns numpy representation of the DF
 array = dataset.to_numpy()
 
-# # input features
+# input features [row_start:row_end, column_start:column_end]
 x = array[:, 0:8]
 
-# # target features
+# target features [row_start:row_end, column_start:column_end]
 y = array[:,8]
 
-# training and test datasets with input and target features. 
+# seperating files into train and test for both input and target features. 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.20, random_state=1, stratify=y)
 
-# #min_max scaling 
-# min_max_scaler = MinMaxScaler()
-# x_train_min_max = min_max_scaler.fit_transform(x_train)
-# x_test_min_max = min_max_scaler.fit_transform(x_test)
+
+# 3. ------------------------ Scaling ------------------------------
+# min_max scaling 
+min_max_scaler = MinMaxScaler()
+x_train_min_max = min_max_scaler.fit_transform(x_train)
+x_test_min_max = min_max_scaler.fit_transform(x_test)
 
 
-# #method for finding nearest odd number 
-# def nearest_odd(n):
-#     n = int(round(n))
-#     return n - 1 if n % 2 == 0 else n
+# function for finding nearest odd number
+def nearest_odd(n):
+    n = int(round(n))
+    return n - 1 if n % 2 == 0 else n
 
-# #deciding k value based on square root of number of samples
-# k = nearest_odd(np.sqrt(len(x_train_min_max)))
+# deciding k value based on square root of number of samples
+# recommendation found at: https://discuss.analyticsvidhya.com/t/how-to-choose-the-value-of-k-in-knn-algorithm/2606/2
+k = nearest_odd(np.sqrt(len(x_train_min_max)))
+
+# if more time remaining, attempt vizualisation for optimal k value shown in link below
+# https://datascience.stackexchange.com/questions/69415/k-nearest-neighbor-classifier-best-k-value
 
 
-# #making predictions
-# knn_model = KNeighborsClassifier(n_neighbors=k)
-# knn_model.fit(x_train_min_max, y_train)
-# knn_predictions = knn_model.predict(x_test_min_max)
+# 4. ------------------------ Algorithm: K-nearest neighbour ----------------------------------------------
+# Instantiate model 
+knn_model = KNeighborsClassifier(n_neighbors=k)
 
-# #evaluating predictions
-# print(accuracy_score(y_test, knn_predictions))
-# print(confusion_matrix(y_test, knn_predictions))
-# print(classification_report(y_test, knn_predictions))
+# Fit model 
+knn_model.fit(x_train_min_max, y_train)
 
-#--------------------------------------Logistic Regression ---------------------------------------------------
+# Prediction
+knn_predictions = knn_model.predict(x_test_min_max)
 
-#Instantiate model (Check that they all have same random state)
+# Model evaluation
+print(accuracy_score(y_test, knn_predictions))
+print(confusion_matrix(y_test, knn_predictions))
+print(classification_report(y_test, knn_predictions))
+
+# 5. --------------------------------------Algorithm: Logistic Regression -----------------------------------
+
+# Instantiate model (Check that they all have same random state)
 logreg_model = LogisticRegression(random_state=1) 
 
-#Fit the model with data
+# Fit the model with data
 logreg_model.fit(x_train, y_train)
 
-#making prediction
+# Prediction
 logreg_output_prediction = logreg_model.predict(x_test)
 
 #Model evaluation
 print(accuracy_score(y_test, logreg_output_prediction))
 print(confusion_matrix(y_test, logreg_output_prediction))
 print(classification_report(y_test, logreg_output_prediction))
-#------------------------------------- Categorical naieve bayes  ---------------------------------------------
+# 6. ------------------------------------- Categorical naieve bayes  ---------------------------------------------
+
 #Category labels
 q_labels = ['low', 'low_med', 'high_med', 'high']
 
@@ -143,7 +139,7 @@ dataset['BMI']  = pd.qcut(dataset['BMI'], q=4, labels=q_labels)
 dataset['diabetes_inheretance_function']  = pd.qcut(dataset['diabetes_inheretance_function'], q=4, labels=q_labels)
 dataset['age']  = pd.qcut(dataset['age'], q=4, labels=q_labels)
 
-
+#Ordinal encoding 
 array2=OrdinalEncoder().fit_transform(dataset)
 
 
