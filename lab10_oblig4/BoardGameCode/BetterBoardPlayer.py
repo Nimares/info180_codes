03daea9ef2a930769e8759874907ed1d086ace15
Nimares <nikolai.resvold@gmail.com>
2023-11-11 23:14:56 +0100

    created a (hopefully) better computer player

--- lab10_oblig4/BoardGameCode/BetterBoardPlayer.py
+++ lab10_oblig4/BoardGameCode/BetterBoardPlayer.py
@@ -1,6 +1,6 @@
 '''
-Class that represent the features and methods for a Simple Computer Board game player
- Programmed by Bjørnar Tessem, Sept-Oct 2022
+Class that represent the features and methods for an Improved  Computer Board game player
+Improved evaluation function. Heuristic now also takes consideration for moves that remove player pieces. 
 '''
 
 
@@ -9,30 +9,34 @@ import Board
 from BoardComputerPlayer import BoardComputerPlayer
 
 
-class SimpleBoardPlayer(BoardComputerPlayer):
+class BetterBoardPlayer(BoardComputerPlayer):
 
     def __init__(self, the_mark):
         '''
         Constructor
         :param compatibility_score_set:
         '''
-        super(SimpleBoardPlayer, self).__init__(the_mark)
+        super(BetterBoardPlayer, self).__init__(the_mark)
         self.name = "Better"
 
     def evaluate_game_status(self, a_board):
         max_cross_row = 0
         max_ring_row = Board.GAMESIZE-1
+        cross_count = 0
+        ring_count = 0
         for i in range(Board.GAMESIZE):
             for j in range(Board.GAMESIZE):
                 if a_board.the_grid[i][j] == CROSS:
+                    cross_count += 0
                     if i > max_cross_row:
                         max_cross_row = i
                 if a_board.the_grid[i][j] == RING:
+                    ring_count += 0
                     if i < max_ring_row:
                         max_ring_row = i
         score = 0
         if self.mark == CROSS:
-            score = max_cross_row - (Board.GAMESIZE-1-max_ring_row)
+            score = max_cross_row - (Board.GAMESIZE-1-max_ring_row) + (cross_count-ring_count)
         if self.mark == RING:
-            score = (Board.GAMESIZE-1-max_ring_row) - max_cross_row
+            score = (Board.GAMESIZE-1-max_ring_row) - max_cross_row + (ring_count-cross_count)
         return score
